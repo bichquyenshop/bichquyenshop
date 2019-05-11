ba28a8395ae3e4762f47564ad99c24fb1e0d6e86
vulinh <linhtv15@fpt.com.vn>
2019-05-11 19:39:28 +0700

    #add jquery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,9 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" type="text/css" href="/css/app.css">
-        <script type="text/javascript" href="/js/app.js"></script>
+        <script type="text/javascript" src="/js/app.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
@@ -22,3 +23,8 @@
         </div>  
     </body>
 </html>
+<script type="text/javascript">
+$(document).ready(function(){
+    alert("SADA");
+});
+</script>
