550bb1bbb0773bb5e6d3a2eb55af031b1d7c9a09
Ngo Cam Tan <bechin28@gmail.com>
2019-05-18 12:17:19 +0700

    fix not login

--- routes/web.php
+++ routes/web.php
@@ -18,46 +18,49 @@ Route::group(['prefix' => '/admin',], function()
     Route::get('/edit-password/{id}', 'AuthController@getEditPassword')->name('users_edit_password');
     Route::post('/edit-password/{id}', 'AuthController@postEditPassword');
 
-    Route::group(['prefix' => '/product',], function()
-    {
-        Route::get('/list','ProductController@getList')->name('list_product');
-        Route::get('add','ProductController@getAdd')->name('add_product');
-        Route::post('add','ProductController@postAdd')->name('add_product');
-        Route::post('delete','ProductController@delete')->name('delete_product');
-        Route::get('edit/{id}', 'ProductController@getEdit')->name('edit_product');
-        Route::post('edit/{id}','ProductController@postEdit')->name('edit_product');
-    });
 
-    Route::group(['prefix' => '/menu',], function()
-    {
-        Route::get('/list','CategoryController@getList')->name('list_menu');
-        Route::get('add','CategoryController@getAdd')->name('add_menu');
-        Route::post('add','CategoryController@postAdd')->name('add_menu');
-        Route::post('delete','CategoryController@delete')->name('delete_menu');
-        Route::get('edit/{id}', 'CategoryController@getEdit')->name('edit_menu');
-        Route::post('edit/{id}','CategoryController@postEdit')->name('edit_menu');
-    });
+    Route::group(['middleware' => ['web','auth'], 'prefix' => '/',], function() {
+        
+        Route::group(['prefix' => '/product',], function()
+        {
+            Route::get('/list','ProductController@getList')->name('list_product');
+            Route::get('add','ProductController@getAdd')->name('add_product');
+            Route::post('add','ProductController@postAdd')->name('add_product');
+            Route::post('delete','ProductController@delete')->name('delete_product');
+            Route::get('edit/{id}', 'ProductController@getEdit')->name('edit_product');
+            Route::post('edit/{id}','ProductController@postEdit')->name('edit_product');
+        });
 
-    Route::group(['prefix' => '/sub-menu',], function()
-    {
-        Route::get('/list','SubCategoryController@getList')->name('list_sub_menu');
-        Route::get('add','SubCategoryController@getAdd')->name('add_sub_menu');
-        Route::post('add','SubCategoryController@postAdd')->name('add_sub_menu');
-        Route::post('delete','SubCategoryController@delete')->name('delete_sub_menu');
-        Route::get('edit/{id}', 'SubCategoryController@getEdit')->name('edit_sub_menu');
-        Route::post('edit/{id}','SubCategoryController@postEdit')->name('edit_sub_menu');
-        Route::post('/getSubCategory','SubCategoryController@getSubCategory')->name('getSubCategory');
-    });
-    Route::group(['prefix' => '/banner',], function()
-    {
-        Route::get('/list','BannerController@getList')->name('list_banner');
-        Route::get('add','BannerController@getAdd')->name('add_banner');
-        Route::post('add','BannerController@postAdd')->name('add_banner');
-        Route::post('delete','BannerController@delete')->name('delete_banner');
-        Route::get('edit/{id}', 'BannerController@getEdit')->name('edit_banner');
-        Route::post('edit/{id}','BannerController@postEdit')->name('edit_banner');
-    });
+        Route::group(['prefix' => '/menu',], function()
+        {
+            Route::get('/list','CategoryController@getList')->name('list_menu');
+            Route::get('add','CategoryController@getAdd')->name('add_menu');
+            Route::post('add','CategoryController@postAdd')->name('add_menu');
+            Route::post('delete','CategoryController@delete')->name('delete_menu');
+            Route::get('edit/{id}', 'CategoryController@getEdit')->name('edit_menu');
+            Route::post('edit/{id}','CategoryController@postEdit')->name('edit_menu');
+        });
 
+        Route::group(['prefix' => '/sub-menu',], function()
+        {
+            Route::get('/list','SubCategoryController@getList')->name('list_sub_menu');
+            Route::get('add','SubCategoryController@getAdd')->name('add_sub_menu');
+            Route::post('add','SubCategoryController@postAdd')->name('add_sub_menu');
+            Route::post('delete','SubCategoryController@delete')->name('delete_sub_menu');
+            Route::get('edit/{id}', 'SubCategoryController@getEdit')->name('edit_sub_menu');
+            Route::post('edit/{id}','SubCategoryController@postEdit')->name('edit_sub_menu');
+            Route::post('/getSubCategory','SubCategoryController@getSubCategory')->name('getSubCategory');
+        });
+        Route::group(['prefix' => '/banner',], function()
+        {
+            Route::get('/list','BannerController@getList')->name('list_banner');
+            Route::get('add','BannerController@getAdd')->name('add_banner');
+            Route::post('add','BannerController@postAdd')->name('add_banner');
+            Route::post('delete','BannerController@delete')->name('delete_banner');
+            Route::get('edit/{id}', 'BannerController@getEdit')->name('edit_banner');
+            Route::post('edit/{id}','BannerController@postEdit')->name('edit_banner');
+        });
+    });
 });
 
 
