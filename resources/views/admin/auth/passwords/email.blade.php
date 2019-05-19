0eeba10b035d45b0ff402d53a31bb523544b72eb
Ngo Cam Tan <bechin28@gmail.com>
2019-05-19 18:58:37 +0700

    design reset pass

--- resources/views/admin/auth/passwords/email.blade.php
+++ resources/views/admin/auth/passwords/email.blade.php
@@ -1,47 +1,50 @@
-@extends('admin.layouts.app')
+@extends('admin.layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="javascript:;">Bích Quyên Jewelry</a>
         </div>
+        <!-- /.login-logo -->
+        <div class="login-box-body">
+            <p class="login-box-msg">Đổi mật khẩu</p>
+
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+
+                <div class=" form-group has-feedback">
+
+                    <input  placeholder="Email"  id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                    @endif
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-6">
+                        <button type="submit" class="btn btn-primary">
+                            Gủi liên kết đổi mật khẩu
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+        <!-- /.login-box-body -->
     </div>
-</div>
-@endsection
+    <!-- /.login-box -->
+@stop
+
+
+
+
