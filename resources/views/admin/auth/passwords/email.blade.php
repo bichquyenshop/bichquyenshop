757fce284c67b9b004ce0558178903f82ed3a1c2
Ngo Cam Tan <bechin28@gmail.com>
2019-05-21 20:19:04 +0700

    update code

--- resources/views/admin/auth/passwords/email.blade.php
+++ resources/views/admin/auth/passwords/email.blade.php
@@ -11,6 +11,8 @@
 
             @if (session('status'))
                 <div class="alert alert-success" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+
                     {{ session('status') }}
                 </div>
             @endif
